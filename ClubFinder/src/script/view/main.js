7bcb4053d15f4122157f1b2fb8485786baf3ae82
rezaduha <rezafauzizisiwaduha@gmail.com>
2020-07-26 17:57:40 +0700

    Add project ClubFinder

---
+++ ClubFinder/src/script/view/main.js
@@ -0,0 +1,26 @@
+import DataSource from "../data/data-source.js";
+import "../components/search-bar.js";
+import "../components/club-list.js";
+
+const main = () => {
+  const searchElement = document.querySelector("search-bar");
+  const clubListElement = document.querySelector("club-list");
+
+  const onButtonSearchClicked = () => {
+    DataSource.searchClub(searchElement.value)
+      .then(renderResult)
+      .catch(fallbackResult);
+  };
+
+  const renderResult = (results) => {
+    clubListElement.clubs = results;
+  };
+
+  const fallbackResult = (message) => {
+    clubListElement.renderError(message);
+  };
+
+  searchElement.clickEvent = onButtonSearchClicked;
+};
+
+export default main;
