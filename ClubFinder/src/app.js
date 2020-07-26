832b71159511c7092fdfc66360d3f64bbc069962
rezaduha <rezafauzizisiwaduha@gmail.com>
2020-07-26 16:20:25 +0700

    Prepare new project DicodingBook

--- ClubFinder/src/app.js
+++
@@ -1,6 +0,0 @@
-import "regenerator-runtime";
-import "./styles/style.css";
-import main from "./script/view/main.js";
-import "./script/components/app-bar.js";
-
-document.addEventListener("DOMContentLoaded", main);
