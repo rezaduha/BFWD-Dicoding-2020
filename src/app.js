d48c8e332fc5aad5e4e0de6e9fc549db4e3354b7
Reza Fauzi Zisiwa Duha <61154073+rezaduha@users.noreply.github.com>
2020-07-29 15:58:09 +0700

    Revert "Add my submission web app to submission branch"

--- src/app.js
+++
@@ -1,7 +0,0 @@
-import "regenerator-runtime";
-import "./styles/style.css";
-import main from "./script/view/main.js";
-import "./script/components/app-bar.js";
-import "./script/components/foot-bar.js";
-
-document.addEventListener("DOMContentLoaded", main);
